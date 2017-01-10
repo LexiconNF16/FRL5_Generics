4a6b3dc8931675d4c4f24ebe8c14200377069080
Adrian Lozano <adrian@xenotype.com>
2017-01-10 11:56:51 +0100

    Add project files.

---
+++ Generics/Pair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Pair<TFirst, TSecond>
+    {
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+        public Pair(TFirst first, TSecond second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+
+    class Pair<T> : Pair<T, T>
+    {
+        public Pair(T first, T second) : base(first, second)
+        {
+        }
+    }
+
+    class Util
+    {
+        public static T Max<T>(T a, T b) where T : IComparable
+        {
+            if (a.CompareTo(b) > 0) return a;
+            return b;
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Util.Max(4, 5));
+            //Console.WriteLine(Util.Max(4.1, 5.2));
+            //Console.WriteLine(Util.Max(4, 5.2));
+            //Console.WriteLine(Util.Max(4.2, 5));
+            //Console.WriteLine(Util.Max("2", "10"));
+            //Console.WriteLine(Util.Max(true, false));
+
+    var isoPair = new Pair<float>(4.3f, 1.9f);
+    var pair = new Pair<int, string>(5, "hej");
+    var first = pair.First;
+    var second = pair.Second;
+
+
+        }
+    }
+
+   
+}
